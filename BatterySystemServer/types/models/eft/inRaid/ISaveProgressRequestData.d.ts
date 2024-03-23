4e0f194457d666e681d0c9459d366c3c763438b1
fijiro <jiro.protonmail@protonmail.com>
2024-03-23 17:34:32 +0200

    Delete unnecessary items from git

--- BatterySystemServer/types/models/eft/inRaid/ISaveProgressRequestData.d.ts
+++
@@ -1,9 +0,0 @@
-import { PlayerRaidEndState } from "../../../models/enums/PlayerRaidEndState";
-import { IPmcData } from "../common/IPmcData";
-import { ISyncHealthRequestData } from "../health/ISyncHealthRequestData";
-export interface ISaveProgressRequestData {
-    exit: PlayerRaidEndState;
-    profile: IPmcData;
-    isPlayerScav: boolean;
-    health: ISyncHealthRequestData;
-}
